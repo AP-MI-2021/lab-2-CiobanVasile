2ee4fd424000595e054cf2dd3f86e89d01ac37fd
Vasi <v.cioban@yahoo.com>
2021-10-01 07:50:50 +0300

    Am rezolvat cele 2 probleme

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 def is_prime(nr):
+    '''
+    -Determina daca un numar este prim sau nu
+    Input:
+    n,numar intre
+    Output:
+    True, n prim
+    False, n neprim
+    '''
     if nr < 2:
         return False
     for i in range(2, nr):
@@ -22,8 +30,38 @@ def get_largest_prime_below(n):
 
     return nr
 
-def get_temp(temp: float,: str, to: str) -> float:
-    return 12.9
+def get_n_choose_k(n: int, k: int):
+    '''
+    -Calculează combinări de n luate câte k
+    Input:
+    n,k,numere intregi,naturale,n>0
+    Output:
+    -Combinarile de n luate cate k
+    '''
+    if n<k:
+        return 0
+    factn=1
+    factk=1
+    factnk=1
+    i=1
+    while i<=n:
+        factn=factn*i
+        i=i+1
+    i=1
+    while i<=k:
+        factk=factk*i
+        i=i+1
+    i=1
+    while i<=n-k:
+        factnk=factnk*i
+        i=i+1
+
+    return factn//(factk*factnk)
+
+
+def test_get_n_choose_k():
+    assert get_n_choose_k(3,2)==3
+    assert get_n_choose_k(10,5)==252
 
 def test_get_largest_prime_below():
     assert get_largest_prime_below(9)==7
@@ -31,6 +69,29 @@ def test_get_largest_prime_below():
     assert get_largest_prime_below(7)==5
 
 
+def main():
+
+  while True:
+    print("Alege problema pe care o vrei rezolvata: ")
+    print("1.Cel mai mare numar strict mai mic decat n")
+    print("2.Combinari de n luate cate k")
+    print("x.Exit")
+
+    nr=input()
+    if nr=='1':
+        n=int(input("Da numarul n: "))
+        print(get_largest_prime_below(n))
+    elif nr=='2':
+        n=int(input("Da numarul n: "))
+        k=int(input("Da numarul k: "))
+        print(get_n_choose_k(n,k))
+    elif nr=='x':
+        break
+    else:
+        print("Optiune gresita")
+
+
 test_get_largest_prime_below()
+test_get_n_choose_k()
 
-
+main()
