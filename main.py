bcb0d208cbde718f5d78edcfed2f0e26728eb94d
Vasi <v.cioban@yahoo.com>
2021-10-05 15:12:04 +0300

    Am lasat doar probleme necesare

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ def is_prime(nr):
     True, n prim
     False, n neprim
     '''
-    if nr < 2:
+    if nr < 2:            #daca numarul este mai mic decat 2 nu este prim
         return False
-    for i in range(2, nr):
+    for i in range(2, nr):#o metoda mai eficienta ar fi sa mergem pana la i*i<=nr
         if nr % i == 0:
             return False
 
@@ -23,7 +23,7 @@ def get_largest_prime_below(n):
     Output:
     -ultimul numar prim mai mic decat n
     '''
-
+    #problema specifica faptul ca ultimul numar prim mai mic decat un numar dat,astfel numarul dat nu se va ferifica chiar daca acesta este prim
     nr=n-1
     while is_prime(nr)==False:
         nr=nr-1
@@ -38,60 +38,54 @@ def get_n_choose_k(n: int, k: int):
     Output:
     -Combinarile de n luate cate k
     '''
-    if n<k:
-        return 0
-    factn=1
-    factk=1
-    factnk=1
-    i=1
-    while i<=n:
-        factn=factn*i
-        i=i+1
-    i=1
-    while i<=k:
-        factk=factk*i
-        i=i+1
-    i=1
-    while i<=n-k:
-        factnk=factnk*i
-        i=i+1
+    #o metoda mai eficienta de calcul a combinarilor cu o singura structura repetitiva
+    c=1
+    for i in range(1,k+1,1):
+          c=c*(n-i+1)//i
 
-    return factn//(factk*factnk)
+    return c
 
-
+# am ales sa dau 5 teste corecte in functiile de test
+# am verificat si posibilele raspunsuri gresit si erori
 def test_get_n_choose_k():
     assert get_n_choose_k(3,2)==3
     assert get_n_choose_k(10,5)==252
+    assert get_n_choose_k(10,3)==120
+    assert get_n_choose_k(3,1)==3
+    assert get_n_choose_k(4,2)==6
 
 def test_get_largest_prime_below():
     assert get_largest_prime_below(9)==7
     assert get_largest_prime_below(12)==11
     assert get_largest_prime_below(7)==5
+    assert get_largest_prime_below(22)==19
+    assert get_largest_prime_below(5)==3
+
+#Testele intotdeauna se vor face inainte de main
+test_get_largest_prime_below()
+test_get_n_choose_k()
+
 
 
 def main():
 
   while True:
-    print("Alege problema pe care o vrei rezolvata: ")
     print("1.Cel mai mare numar strict mai mic decat n")
     print("2.Combinari de n luate cate k")
     print("x.Exit")
 
-    nr=input()
-    if nr=='1':
+    option=input("Alege problema pe care o vrei rezolvata: ")
+    if option=='1':
         n=int(input("Da numarul n: "))
         print(get_largest_prime_below(n))
-    elif nr=='2':
+    elif option=='2':
         n=int(input("Da numarul n: "))
         k=int(input("Da numarul k: "))
         print(get_n_choose_k(n,k))
-    elif nr=='x':
+    elif option=='x':
         break
     else:
         print("Optiune gresita")
 
-
-test_get_largest_prime_below()
-test_get_n_choose_k()
 
 main()
